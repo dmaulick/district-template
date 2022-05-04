58d09c3dbad213ac9f00b676b1bea2a5e49ffdb9
David Maulick <davidmaulick@gmail.com>
2022-05-04 15:27:19 -0700

    custom path aliasing

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            tests: ['./tests/'],
+            "@components": "./src/components",
+            "@screens": ["./src/screens"],
+            "@hooks": ["./src/hooks"],
+          }
+        }
+      ]
+    ]
   };
 };
