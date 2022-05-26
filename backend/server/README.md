50bc9b77e6d989ef817ff22d2101e0653e98b366
David Maulick <davidmaulick@gmail.com>
2022-05-25 21:15:56 -0700

    graphql and mongodb working now

---
+++ backend/server/README.md
@@ -0,0 +1,3 @@
+Good Basic tutorial:
+https://javascript.plainenglish.io/lets-build-a-mongodb-express-react-graphql-relay-modern-full-stack-web-app-from-scratch-a4ae3aaf212
+
--- backend/server/app.js
+++ backend/server/app.js
@@ -1,25 +1,21 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./graphql/schema');
+const { setupDB } = require('./config/databaseConnection');
+const cors = require('cors');
 
-const { setupDB, getDB } = require('./config/databaseConnection');
-
+const app = express();
 setupDB(v => console.log(v));
 
-
-const schema = {
-  // we will add this later
-};
-
 app.use(cors());
+
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-    pretty: true
-  })
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+        pretty: true
+    })
 );
 
 app.listen(4000);
